62f9162a9556d295c361f3cdea7beb33bf9eb0d6
FRANCISCO Savastano <fransavastanocp@gmail.com>
2024-02-06 15:58:23 -0300

    feat(francisco): adicionada get de info dos lobos

--- PáginaShowLobinho/show_lobinho.js
+++ PáginaShowLobinho/show_lobinho.js
@@ -1,12 +1,22 @@
+import { fetchJSONData } from "../fetch.js";
+//Alterar isso para o item correto
+//Recebe o id do lobo da pagina de lista
 async function getData() {
     let data = await fetchJSONData();
-    let obj = data[0]
-    return obj
+    return data
 }
+async function getLobo() {
+    let loboid = localStorage.getItem("inputValue");
+    loboid = 2
+    getData().then(dados =>{
+        let lobo = dados[loboid - 1]
+        nomelobo.innerText = lobo.nome;
+        desclobo.innerHTML = lobo.descricao;
+        imglobo.setAttribute("src", lobo.imagem)
 
-let obj = getData()
-
-obj.then(obj => {
-    let nome = obj.nome;
-    document.getElementById("teste").innerHTML
-})
+    })
+}
+var nomelobo = document.getElementById("nomelobo")
+var desclobo = document.getElementById("desclobo")
+var imglobo = document.getElementById("imglobo")
+getLobo()
