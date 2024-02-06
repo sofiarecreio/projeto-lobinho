8e5da880b553fec79551b3ff5ce05309d9976fe5
FRANCISCO Savastano <fransavastanocp@gmail.com>
2024-02-06 13:01:04 -0300

    feat(francisco): add pagina show lobinho

--- PáginaShowLobinho/show_lobinho.js
+++ PáginaShowLobinho/show_lobinho.js
@@ -1,5 +1,3 @@
-import { fetchJSONData } from "../fetch.js";
-//Como estamos lidando com uma promise, a função precisa ser acessada com uso de .then(data => ) para acessar os dados dela
 async function getData() {
     let data = await fetchJSONData();
     let obj = data[0]
