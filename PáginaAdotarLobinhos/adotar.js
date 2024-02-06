58e8402d8c3d2cd4391c70dd666a8aa0b67db3b6
FRANCISCO Savastano <fransavastanocp@gmail.com>
2024-02-06 11:12:22 -0300

    refactor(francisco): alterado modo de invocação da função em todos .js

--- PáginaAdotarLobinhos/adotar.js
+++ PáginaAdotarLobinhos/adotar.js
@@ -1,4 +1,3 @@
 import { fetchJSONData } from "../fetch.js";
-
-let res = fetchJSONData();
-console.log(res)
+//Como estamos lidando com uma promise, a função precisa ser acessada com uso de .then(data => ) para acessar os dados dela
+fetchJSONData().then(data => console.log(data)); 
--- PáginaQuemSomos/quem_somos.js
+++ PáginaQuemSomos/quem_somos.js
@@ -1,3 +1,3 @@
 import { fetchJSONData } from "../fetch.js";
-
-fetchJSONData();
+//Como estamos lidando com uma promise, a função precisa ser acessada com uso de .then(data => ) para acessar os dados dela
+fetchJSONData().then(data => console.log(data)); 
