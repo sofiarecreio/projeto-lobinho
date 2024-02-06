779db424b335d9fb6dd48571f49acc8fc9d8e3c9
ndanilop <danilodn993@gmail.com>
2024-02-06 14:59:06 -0300

    fix(danilo): formulario e estilizacao

--- PáginaAdicionarLobinho/adicionar.js
+++ PáginaAdicionarLobinho/adicionar.js
@@ -1,3 +1,8 @@
 import { fetchJSONData } from "../fetch.js";
 //Como estamos lidando com uma promise, a função precisa ser acessada com uso de .then(data => ) para acessar os dados dela
 fetchJSONData().then(data => console.log(data)); 
+
+
+
+
+const btn_enviar = document.querySelector
