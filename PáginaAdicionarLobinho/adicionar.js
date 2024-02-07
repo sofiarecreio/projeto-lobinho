b96d1de2409e3f35031293e6139bc76eae07386a
ndanilop <danilodn993@gmail.com>
2024-02-07 16:19:46 -0300

    fix(danilo): funcionalidade pag adicionar

--- PáginaAdicionarLobinho/adicionar.js
+++ PáginaAdicionarLobinho/adicionar.js
@@ -1,3 +1,4 @@
+import { fetchJSONData } from "../fetch.js";
 //Como estamos lidando com uma promise, a função precisa ser acessada com uso de .then(data => ) para acessar os dados dela
 fetchJSONData().then(data => console.log(data)); 
 
@@ -6,9 +7,9 @@ async function getData() {
     return data
 }
 
+// adicionando  lobinho
+async function PostWolf(data){
 
-// adicionando  lobinhos
-async function PostWolf(){
     //selecionando os valores do formulário
     let nome = document.querySelector("#nome").value;
     let idade = document.querySelector("#idade").value;
@@ -16,32 +17,22 @@ async function PostWolf(){
     let descricao = document.querySelector(".descricao").value;
 
     // casos inválidos
-    if(nome == ""){
-        alert("preencha o nome corretamente")
+    if(nome == "" || idade == "" || isNaN(idade) == true || link_foto == "" || descricao == ""){
+        alert("preencha todos os campos corretamente")
     }else{
-        if(idade == "" || isNaN(idade) == true){
-            alert("preencha a idade corretamente (somente números)")
-        }else{
-            if(link_foto == ""){
-                alert("preencha o link corretamente")
-            }else{
-                if(descricao == ""){
-                    alert("preencha a descricao corretamente")
-                }else{
-                    //caso todos os campos estejam válidos
-                    //criando um dicionario com os valores digitados nos campos
-                    let dados = {
-                        "id" : (dados.lenght+1),
-                        "nome": nome,
-                        "idade": idade,
-                        "descricao": descricao,
-                        "imagem": link_foto
-                    };
-                    //adicionando na array 
-                    data.push(JSON.stringify(dados));
-                }
-            }
-        }
+        //criando um lobo e adicionando seus dados
+        let dados_lobo = {
+        "id" : (data.lenght+1),
+        "nome": nome,
+        "idade": idade,
+        "descricao": descricao,
+        "imagem": link_foto,
+        "adotado": false,
+        "nomeDono": null,
+        "idadeDono": null
+        };
+
+        data.push(dados_lobo);
     }
 }
 
@@ -49,14 +40,12 @@ async function PostWolf(){
 const btn_salvar = document.querySelector(".btn_salvar")
 
 //ação de apertar o botao
-btn_salvar.addEventListener("click",()=>{getData()
-    .then(data => {
-        console.log(data);
-        PostWolf()
-    })
-    .catch(error => {
-        console.error("Ocorreu um erro:", error);
-    })
+btn_salvar.addEventListener("click", async () => {
+    try {
+        let data = await getData();
+        await PostWolf(data);
+    } catch (error) {
+        console.log("ocorreu um erro");
+        console.log(error);
+    }
 });
-
-
