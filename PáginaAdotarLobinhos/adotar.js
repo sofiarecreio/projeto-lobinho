62153e18e321ef67852ba0067f03a564aeb4adb2
Rafael de Lima Pereira <devrafalime@proton.me>
2024-02-07 20:55:55 -0300

    feat(adotar): Adicionado informações originadas de outras páginas

--- PáginaAdotarLobinhos/adotar.js
+++ PáginaAdotarLobinhos/adotar.js
@@ -1,17 +1,26 @@
 //Como estamos lidando com uma promise, a função precisa ser acessada com uso de .then(data => ) para acessar os dados dela
-async function getData() {
+import { fetchJSONData } from "../fetch.js";
+
+
+async function getData(id) {
     let data = await fetchJSONData();
-    let obj = data[0]
+    let obj = data[id]
     return obj
 }
 
-let obj = getData()
 
-obj.then(obj => {
-    let nome = obj.nome;
-    document.getElementById("teste").innerHTML
-})
 
 // Pegando dados vindos da url de redirecionamento se houver
 var parametros = new URLSearchParams(window.location.search)
 var id = parametros.get('id')
+
+let ID = document.querySelector(".subtitulo p")
+if (!(id === null)) {
+    ID.innerHTML = `ID:${id}`
+}
+
+let pic = document.querySelector(".img img")
+getData(id).then((data) => {
+    pic.setAttribute("src", data.imagem)
+
+})
